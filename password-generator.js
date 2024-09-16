dd6b9c2c6685a5deec1236870b1b8654c765d6f7
Allison <Allison.Boone@keyin.com>
2024-09-16 17:17:13 -0230

    Added customizable options in seperate branch

--- password-generator.js
+++ password-generator.js
@@ -4,18 +4,41 @@
 const argv = require('yargs/yargs')(process.argv.slice(2))
 
   //Creating flags and default option (8 being default length of password).
+  //Later edited in separate branch to add customizable options.
   .usage('Usage: $0 [options')
   .help('h')
   .alias('h', 'help')
   .option('l', {
     alias: 'length',
-    describe: 'Choose desired length of password',
+    describe: 'Choose desired length of password.',
     type: 'number',
     default: 8,
+  })
+  .option('n', {
+    alias: 'numbers',
+    describe: 'Allow numbers to be included in password.',
+    type: 'boolean',
+    default: false,
+  })
+  .option('u', {
+    alias: 'uppercase',
+    describe: ' Allow uppercase letters to be included in password.',
+    type: 'boolean',
+    default: false,
+  })
+  .option('s', {
+    alias: 'symbols',
+    describe: 'Allow symbols to be included in password.',
+    type: 'boolean',
+    default: false,
   }).argv;
 
 //Allowable default characters.
-const characters = 'abcdefghijklmnopqrstuvwxyz';
+//Later Later edited in separate branch to add customizable option characters.
+const characters = {
+  lowerCase: 'abcdefghijklmnopqrstuvwxyz',
+  upperCase: 'ABCDEFGHIJKLNMOPQRSTUVWXYZ',
+};
 
 //Function to create random password using password length and allowable characters.
 function generatedPassword(length) {
