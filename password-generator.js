0534b8ffcb5c29b0464c4c4ed50e21f50805e778
Allison <Allison.Boone@keyin.com>
2024-09-16 16:57:39 -0230

    created function to create password with a given length from the command line, displayed password in the console

--- password-generator.js
+++ password-generator.js
@@ -13,3 +13,24 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
     type: 'number',
     default: 8,
   }).argv;
+
+//Allowable default characters.
+const characters = 'abcdefghijklmnopqrstuvwxyz';
+
+//Function to create random password using password length and allowable characters.
+function generatedPassword(length) {
+  let password = '';
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+}
+
+//Gets password length from the command line and then Generates password.
+const passwordLength = args.length;
+const password = generatedPassword(passwordLength);
+
+// Displays randomly generated password in the console.
+console.log(`Randomly generated password: ${password}`);
